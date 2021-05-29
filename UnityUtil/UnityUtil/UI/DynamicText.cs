b9431c7ab793c7117e1c58bae206ba7e77ecd4e5
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2021-05-29 11:46:22 -0300

    initial

---
+++ UnityUtil/UnityUtil/UI/DynamicText.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DynamicText : MonoBehaviour
+{
+    private Text text;
+
+    private void Awake()
+    {
+        Initialize();
+    }
+    void Start()
+    {
+        Initialize();
+    }
+    public void UpdateText(int value)
+    {
+        text.text = value.ToString();
+    }
+    public void UpdateText(string value)
+    {
+        text.text = value;
+    }
+
+    public void UpdateText(float value, string format = "0.00")
+    {
+        text.text = value.ToString(format);
+    }
+
+    public void Initialize()
+    {
+        text = GetComponent<Text>();
+        if (text == null)
+            Debug.Log("Text component not found");
+    }
+}
